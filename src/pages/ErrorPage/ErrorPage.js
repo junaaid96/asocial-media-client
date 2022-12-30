87edd0661e300c0f40fa424c8a5655e67df9990c
junaaid96 <info.junaidulislam@gmail.com>
2022-12-30 15:04:45 +0600

    error page added

--- src/pages/ErrorPage/ErrorPage.js
+++ src/pages/ErrorPage/ErrorPage.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import errorGif from "../../assets/gif/Error.gif";
 
 const ErrorPage = () => {
     return (
-        <div>
-            Error!
+        <div className="flex flex-col justify-center items-center">
+            <div>
+                <img src={errorGif} alt="error" />
+            </div>
+            <div>
+                <Link to="/">
+                    <button className="btn btn-primary">Go to Home</button>
+                </Link>
+            </div>
         </div>
     );
 };
