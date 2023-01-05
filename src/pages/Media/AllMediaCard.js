37e4177273b62316525200f1e0f15b6e48918882
junaaid96 <info.junaidulislam@gmail.com>
2023-01-05 22:05:47 +0600

    user can add comments

--- src/pages/Media/AllMediaCard.js
+++ src/pages/Media/AllMediaCard.js
@@ -1,10 +1,60 @@
+import { useQuery } from "@tanstack/react-query";
 import React, { useContext } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "react-hot-toast";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthProvider";
+import LoadingScreen from "../LoadingScreen/LoadingScreen";
 
 const AllMediaCard = ({ singlePost }) => {
     const { user } = useContext(AuthContext);
-    const { username, writings, photo } = singlePost;
+    const { _id, username, writings, photo } = singlePost;
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        reset,
+    } = useForm();
+
+    const {
+        data: userComments = [],
+        isLoading,
+        refetch,
+    } = useQuery({
+        queryKey: ["userComments", _id],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/comments/${_id}`);
+            const data = await res.json();
+            console.log(data);
+            return data;
+        },
+    });
+
+    if (isLoading) {
+        <LoadingScreen />;
+    }
+
+    //add comment
+    const handleAddComment = (data) => {
+        const comment = {
+            post_id: _id,
+            username: user.displayName,
+            email: user.email,
+            comment: data.comment,
+        };
+        console.log(comment);
+        fetch("http://localhost:5000/comments", {
+            method: "POST",
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify(comment),
+        }).then((res) => {
+            if (res.status === 200) {
+                refetch();
+                reset();
+                toast.success("Comment added successfully!");
+            }
+        });
+    };
     return (
         <div className="card h-fit bg-black shadow-xl">
             <figure>
@@ -27,16 +77,52 @@ const AllMediaCard = ({ singlePost }) => {
                                 0
                             </span>
                         </div>
-                        <div className="flex items-center gap-3 w-full">
-                            <textarea
-                                type="text"
-                                className="input input-bordered w-full"
-                                placeholder="add a comment"
-                            ></textarea>
-                            <button className="btn btn-outline btn-primary btn-sm">
-                                Post
-                            </button>
+                        <div className="w-full">
+                            <form
+                                className="flex gap-1 items-center3"
+                                onSubmit={handleSubmit(handleAddComment)}
+                            >
+                                <div className="w-full">
+                                    <input
+                                        {...register("comment", {
+                                            required: "Comment is Required",
+                                        })}
+                                        type="text"
+                                        placeholder="add a comment"
+                                        className="input input-bordered w-full"
+                                    />
+                                    {errors.comment && (
+                                        <p className="text-red-500">
+                                            {errors.comment.message}
+                                        </p>
+                                    )}
+                                </div>
+                                <button
+                                    type="submit"
+                                    className="btn btn-primary btn-sm"
+                                >
+                                    Post
+                                </button>
+                            </form>
                         </div>
+                        {
+                            //show comments
+                            userComments.length > 0 && (
+                                <div className="h-fit w-full rounded-lg p-3 bg-gray-800 bg-opacity-50 flex flex-col gap-3">
+                                    {userComments.map((userComment) => (
+                                        <div
+                                            key={userComment._id}
+                                            className="bg-gray-600 bg-opacity-50 p-3 rounded-r-lg"
+                                        >
+                                            <p className="font-bold">
+                                                {userComment.username}
+                                            </p>
+                                            <p>{userComment.comment}</p>
+                                        </div>
+                                    ))}
+                                </div>
+                            )
+                        }
                     </div>
                 ) : (
                     <div className="card-actions flex-col gap-6">
